344122cc5ee895c042c60728b3bc5aba831619de
Waldener Monteiro <jjrr019@gmail.com>
2020-07-31 13:29:13 -0300

    feature: instalando na aplicação o vuetify, configurando eslint e afins

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import store from './store'
 import Vuelidate from 'vuelidate'
 import VueLoading from 'vuejs-loading-plugin'
 import baseVuex from './boot/baseVuex'
+import vuetify from './plugins/vuetify'
 Vue.use(Vuelidate)
 Vue.use(VueLoading, { text: 'Carregando' })
 Vue.use(baseVuex)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
