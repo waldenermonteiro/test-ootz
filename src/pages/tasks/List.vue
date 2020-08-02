6d906cec9d14b8626e91274894a590f8d831e2ef
Waldener Monteiro <jjrr019@gmail.com>
2020-08-02 16:20:12 -0300

    feature(tasks): criando listagem de tarefas

---
+++ src/pages/tasks/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-list color="#f6f6f4">
+    <draggable :list="list" ghost-class="ghost" @start="dragging = true" @end="dragging = false" v-bind="{ group: 'tasks-group' }">
+      <transition-group class="list-custom" type="transition" name="flip-list">
+        <v-list-item v-for="task in list" :key="task.id" class="list-group-item">
+          <v-card style="width: 100%">
+            <v-list-item-content>
+              <p class="list-item-title">{{ task.title }}</p>
+              <!-- <v-list-item-title v-text="task.title"></v-list-item-title> -->
+            </v-list-item-content>
+          </v-card>
+        </v-list-item>
+      </transition-group>
+    </draggable>
+  </v-list>
+</template>
+<script>
+import Draggable from 'vuedraggable'
+export default {
+  components: {
+    Draggable
+  },
+  props: {
+    list: {
+      required: false
+    }
+  }
+}
+</script>
