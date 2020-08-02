244aa3dc993b513c4e62fe8e70deedf4a03066cc
Waldener Monteiro <jjrr019@gmail.com>
2020-08-02 16:21:21 -0300

    refactor; definindo primeiramente o css do projeto no app, depois será passado para os arquivos

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,29 @@
 html {
   overflow: auto !important;
 }
+.ghost {
+  opacity: 0.2;
+  background: #6200ea !important;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: pointer;
+}
+.list-custom .v-list-item {
+  min-height: 30px !important;
+  margin: 5px !important;
+  padding: 0px 1px !important;
+  margin-bottom: 10px !important;
+  // VERIFICAR DE NOVO POIS ADICIONOU A BORDA ONDE NÃO DEVERIA
+}
+.list-item-title {
+  padding: 0px 8px !important;
+  margin: 0px !important;
+}
 </style>
