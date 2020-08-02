21faa0f2b67c2f4bf13cb3d9b5288f51cd067a18
Waldener Monteiro <jjrr019@gmail.com>
2020-08-02 16:20:41 -0300

    feature(panels): criando componente de criação de painéis

---
+++ src/pages/panels/Create.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="deep-purple accent-4 white--text">
+          Create Panel
+        </v-card-title>
+
+        <v-card-text>
+          <v-text-field @keyup.enter="create()" label="Add title panel" v-model="form.text"></v-text-field>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="closeModal()">
+            Create Panel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+      form: {
+        text: ''
+      }
+    }
+  },
+  methods: {
+    openModal () {
+      this.dialog = true
+    },
+    closeModal () {
+      this.dialog = false
+    },
+    create () {}
+  }
+}
+</script>
