b2ab949ef9ceaa370c83b0c19f45e2a8937f0bbe
Waldener Monteiro <jjrr019@gmail.com>
2020-08-05 21:05:14 -0300

    refactor(frames): adicionando método para sobrescrever o base

--- src/pages/frames/services/FrameService.js
+++ src/pages/frames/services/FrameService.js
@@ -1,7 +1,16 @@
 import Base from '../../../services/base'
 class FrameService extends Base {
   constructor () {
-    super('/frames')
+    super('/frame')
   }
+
+  list = async ($params = '') => {
+    try {
+      const { data } = await this.service.get('frames', { params: $params })
+      return data
+    } catch (error) {
+      throw this.statusResponse(error, 'list')
+    }
+  };
 }
 export default new FrameService()
